d6e17ec36ab10c8097c90ff9e92392be1a4180ba
RAVOAHANGILALAO Anjaniaina Kaloina Melodie <99157194+KaloinaMelodie@users.noreply.github.com>
2026-03-17 13:31:58 +0300

    Update cleaning.js

--- rental-back/src/models/cleaning.js
+++ rental-back/src/models/cleaning.js
@@ -5,14 +5,14 @@ class Cleaning {
     new_booking_id;
     new_booking_date;
     conflict;
-    
-    constructor(property_id, checkout_booking_id, checkout_booking_date, new_booking_id, new_booking_date, conflict){
-        this.property_id =property_id;
-        this.checkout_booking_date = checkout_booking_date;
-        this.checkout_booking_id = checkout_booking_id;
-        this.new_booking_date = new_booking_date;
-        this.new_booking_id = new_booking_id;
-        this.conflict = conflict;
-    }    
+
+    // constructor(property_id, checkout_booking_id, checkout_booking_date, new_booking_id, new_booking_date, conflict){
+    //     this.property_id =property_id;
+    //     this.checkout_booking_date = checkout_booking_date;
+    //     this.checkout_booking_id = checkout_booking_id;
+    //     this.new_booking_date = new_booking_date;
+    //     this.new_booking_id = new_booking_id;
+    //     this.conflict = conflict;
+    // }    
 
 }
